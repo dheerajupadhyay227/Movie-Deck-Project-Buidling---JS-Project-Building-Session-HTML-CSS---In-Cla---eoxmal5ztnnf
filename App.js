6377e22e02a3d200dd75b6b305a3dc7bb708391d
Dheeraj Upadhyay <131439120+dheerajupadhyay227@users.noreply.github.com>
2023-08-18 15:40:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,121 +1,200 @@
-const API_KEY = '4da49863-91c1-4613-a6f0-bbbbd2349b00';
-const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
-const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
-const API_URL_MOVIE_DETAILS = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
+let pageNumber;
+let totalPage = 0;
+let sortBtnByReleaseDate = document.querySelector('#sort-date');
+let sortBtnByPopularity = document.querySelector('#sort-rate');
+let movieList = document.querySelector('#movie-list');
+let nextPageBtn = document.querySelector('#next-page');
+let prevPageBtn = document.querySelector('#prev-page');
+let currPageBtn = document.querySelector('#curr-page');
+let showFavoriteMovies = document.querySelector('#show-fav');
+let shwoAllMovies = document.querySelector('#show-all');
+let favoriteMoviesArray = [];
+let favoriteMoviesIdArray = [];
+let sortByProperty = "vote_count.desc";
 
-getMovies(API_URL_POPULAR)
+async function getData(pageNumber, sort){
 
-async function getMovies(url) {
-    const response = await fetch(url, {
-        headers:{
-            'X-API-KEY': '4da49863-91c1-4613-a6f0-bbbbd2349b00',
-            'Content-Type': API_KEY,
-        },
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGNiMTY3YWY0YjM4MjZlNmIyMzRjOGZmZGI2YTg3ZCIsInN1YiI6IjY0ZDM4NWFmZGI0ZWQ2MDBlMmI0ZjhjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jgY8TgBak84vKO3UJK0mmxDiVWo8W5EHNDoOGqOJQ6o'
+        }
+    };
+      
+    let response = await fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${pageNumber}&sort_by=${sort}`, options)
+    let dataObj = await response.json();
+    movieList.innerHTML = "";
+    totalPage = dataObj.total_pages;
+    
+    dataObj.results.forEach((element)=>{
+        movieList.innerHTML += `<section class="movies-card" id="${element.id}">
+        <section class="poster">
+            <img src="https://image.tmdb.org/t/p/w300${element.backdrop_path}" alt="Oops something broke..." class="movie-poster"/>
+        </section>
+        <h2 class="movie-title">${element.title}</h2>
+        <footer class="movie-card--footer">
+            <section class="movie-rating">
+                <p class="movie--vote-count">Vote count: ${element.vote_count}</p>
+                <p class="movie--vote-avg">Average vote: ${element.vote_average}</p>
+            </section>
+            <section class="fav-movie">
+                <i class="fa-regular fa-heart fav-movie-icon"></i>
+            </section>
+        </footer>
+    </section>`;
+    })
+    
+    addHeartToFavMovie();
+    // Updated current page Number
+    currPageBtn.innerText = `Current Page: ${pageNumber}`
+}
+// Displayed sorted movies by release date
+function displaySortByReleaseDate(){
+    sortBtnByReleaseDate.addEventListener('click', ()=> {
+
+        if(sortByProperty !== "primary_release_date.desc"){
+            sortByProperty ="primary_release_date.desc";
+            sortBtnByReleaseDate.innerHTML = `Sort by date <i class="fa-solid fa-arrow-down">`
+        }
+        else{
+            sortByProperty= 'primary_release_date.asc';
+            sortBtnByReleaseDate.innerHTML = `Sort by date <i class="fa-solid fa-arrow-up">`
+        }
+        sortBtnByPopularity.innerHTML = `Sort by rating`
+        paginationShow()
+        getData(1, sortByProperty);
+    })
+}
+// Displayed sorted movies by popularity
+function displaySortByPopularity(){
+    sortBtnByPopularity.addEventListener('click', () => {
+
+        if(sortByProperty === "vote_count.desc"){
+            sortByProperty = "vote_count.asc"
+            sortBtnByPopularity.innerHTML = `Sort by rating <i class="fa-solid fa-arrow-up">`
+        }
+        else{
+            sortByProperty="vote_count.desc";
+            sortBtnByPopularity.innerHTML = `Sort by rating <i class="fa-solid fa-arrow-down">`
+        }
+        sortBtnByReleaseDate.innerHTML = `Sort by date`
+        paginationShow();
+        getData(1,sortByProperty);
+    })
+}
+// Showed All Movies when clicked on All button
+function displayAllMovies(){
+    shwoAllMovies.addEventListener('click',()=>{
+        if(!shwoAllMovies.classList.contains('active-tab')){
+            showFavoriteMovies.classList.toggle('active-tab');
+            shwoAllMovies.classList.toggle('active-tab');
+        }
+        sortBtnByPopularity.innerHTML = `Sort by rating`
+        sortBtnByReleaseDate.innerHTML = `Sort by date`
+        sortByProperty="vote_count.desc"
+        pageNumber = 1;
+        paginationShow();
+        getData(pageNumber,sortByProperty);
+
+    })
+};
+// Toggled the heart icon to solid or regular based on whether its already added as a favorite(When page load)
+function addHeartToFavMovie(){
+
+    movieList.childNodes.forEach((element)=>{
+        let id = element.id;
+        let index = favoriteMoviesIdArray.indexOf(id);
+        if(index !== -1){
+            let like = element.childNodes[5].lastChild.previousSibling.childNodes[1];
+            like.classList.toggle("fa-solid");
+        }
+    })
+};
+// Added Favorite movies card and it's ids in an Array of Favorite movies
+function favoriteMovieList(){
+
+    movieList.addEventListener('click',(event)=>{
+        if(event.target.classList.contains('fav-movie-icon')){
+            let target = event.target;
+            
+            target.classList.add("fa-regular"); // getting bug when using target.classList.toggle("fa-regular");
+            target.classList.toggle('fa-solid');
+            let id = target.parentNode.parentNode.parentNode.id;
+            if(/fa-solid/.test(target.className)){
+                favoriteMoviesIdArray.unshift(id);
+                favoriteMoviesArray.unshift(target.parentNode.parentNode.parentNode)
+            }
+            else{
+                let index = favoriteMoviesIdArray.indexOf(id);
+                favoriteMoviesIdArray.splice(index, 1)
+                favoriteMoviesArray.splice(index,1 )
+            }
+        }
     });
-    const responseData = await response.json();
-    showMovies(responseData);
+};
+// Displayed Favorite Movies when clicked on Favorite button
+function displayFavoriteMovies(){
+
+    showFavoriteMovies.addEventListener('click',()=>{
+        paginationHide()
+        sortBtnByPopularity.innerHTML = `Sort by rating`
+        sortBtnByReleaseDate.innerHTML = `Sort by date`
+        if(!showFavoriteMovies.classList.contains('active-tab')){
+            showFavoriteMovies.classList.toggle('active-tab');
+            shwoAllMovies.classList.toggle('active-tab');
+        }
+        if(favoriteMoviesArray.length == 0){
+            movieList.innerHTML = "No movies added to your favorites yet";
+            return;
+        }
+        movieList.innerHTML ="";
+        favoriteMoviesArray.forEach((element)=>{
+            movieList.appendChild(element);
+        })
+        
+    })
+};
+// Loaded Next or Previous page
+function addNavigationButtons(){
+
+    // Loaded next page
+    nextPageBtn.addEventListener('click',()=>{
+        if(totalPage > pageNumber){
+            pageNumber++;
+            getData(pageNumber,sortByProperty)
+        }
+        
+    });
+    // Loaded previous page
+    prevPageBtn.addEventListener('click',()=>{
+        if (pageNumber > 1 ){
+            pageNumber--;
+            getData(pageNumber,sortByProperty)
+        }
+
+    })
+};
+function paginationHide(){
+    nextPageBtn.style.visibility = 'hidden'
+    prevPageBtn.style.visibility = 'hidden'
+    currPageBtn.style.visibility = 'hidden'
+}
+function paginationShow(){
+    nextPageBtn.style.visibility = 'visible'
+    prevPageBtn.style.visibility = 'visible'
+    currPageBtn.style.visibility = 'visible'
 }
 
-function getClassByRate(vote){
-    if(vote >= 7){
-        return 'green'
-    } else if (vote > 5) {
-        return 'orange'
-    }else{
-        return 'red'
-    }
-}
-
-function showMovies(data) {
-    const movies = document.querySelector('.movies');
-
-    document.querySelector('.movies').innerHTML = '';
-
-    data.films.forEach((movie) => {
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-        <div class="movie__cover-inner">
-            <img src="${movie.posterUrlPreview}" alt="${movie.nameRu}" class="movie__cover">
-            <div class="movie__cover--darkened"></div>
-        </div>
-        <div class="movie__info">
-            <div class="movie__title">${movie.nameRu}</div>
-            <div class="movie__category">${movie.genres.map((genres) => ` ${genres.genre}`)}</div>
-            <div class="movie__average movie__average--${getClassByRate(movie.rating)}">${movie.rating}</div>
-        </div>
-        `;
-
-        movieEl.addEventListener('click', () => openModal(movie.filmId))
-
-        movies.appendChild(movieEl);
-    });
+// Initialized the page
+async function init(){
+    pageNumber = 1;
+    await getData(1,sortByProperty);
+    displaySortByReleaseDate()
+    displaySortByPopularity()
+    addNavigationButtons();
+    favoriteMovieList();
+    displayFavoriteMovies();
+    displayAllMovies();
 };
-
-const form = document.querySelector('form');
-const search = document.querySelector('.header__search');
-const btn = document.querySelector('.header__btn')
-
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`
-    if(search.value) {
-        getMovies(apiSearchUrl);
-
-        search.value = '';
-    }
-});
-
-// Modal
-let modalEl = document.querySelector('.modal');
-
-async function openModal(id) {
-    const response = await fetch(API_URL_MOVIE_DETAILS + id, {
-        headers:{
-            'X-API-KEY': '4da49863-91c1-4613-a6f0-bbbbd2349b00',
-            'Content-Type': API_KEY,
-        },
-    });
-    const responseData = await response.json();
-
-    modalEl.classList.add('modal--show');
-    document.body.classList.add('stop-scrolling');
-
-    modalEl.innerHTML = `
-    <div class="modal__card">
-        <img src="${responseData.posterUrl}" alt="" class="modal__movie-backdrop">
-        <h2>
-            <span class="modal__movie-title">Name - ${responseData.nameRu}</span>
-            <span class="modal__movie-release-year">${responseData.year}</span>
-        </h2>
-        <ul class="modal__movie-info">
-            <div class="loader"></div>
-            <li class="modal__movie-genre">Genre - ${responseData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
-            <li class="modal__movie-runtime">Time - ${responseData.filmLength} минут</li>
-            <li>Site: <a href="${responseData.webUrl}" class="modal__movie-site">${responseData.webUrl}</a></li>
-            <li class="modal__movie-overview">Description - ${responseData.description}</li>
-        </ul>
-        <button type="button" class="modal__button-close">close</button>
-    </div>
-    `
-    const btnClose = document.querySelector('.modal__button-close');
-    btnClose.addEventListener('click', () => closeModal());
-};
-
-function closeModal() {
-    modalEl.classList.remove('modal--show');
-    document.body.classList.remove('stop-scrolling');
-};
-
-window.addEventListener('click', (e) => {
-    if(e.target === modalEl) {
-        closeModal();
-    }
-});
-
-window.addEventListener('keydown', (e) => {
-    if(e.keyCode === 27){
-        closeModal();
-    }
-});
+init();
